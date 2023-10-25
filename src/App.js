db3a4240e550476fd548c964317c2ca07f2521c6
soolovepat <soolovepat@naver.com>
2023-10-25 22:38:04 +0900

    feat TodoList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 function App() {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 }
