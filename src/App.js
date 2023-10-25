0a1f23445a9c0a6e896c96550fd078c6ea57cfb4
soolovepat <soolovepat@naver.com>
2023-10-25 23:10:32 +0900

    feat TodoList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '호빵 찌기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '감자 찌기',
+      checked: true,
+    },
+    { id: 3, text: '붕어빵 굽기', checked: false },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
