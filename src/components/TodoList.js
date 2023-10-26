c1fc9d76dfa689a29e1438f7b47b3c8dac1a4316
soolovepat <soolovepat@naver.com>
2023-10-26 15:03:42 +0900

    update virtualized List

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import TodoListItem from './TodoListItem';
+import { List } from 'react-virtualized';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="TodoList">
-      {todos.map((todo) => (
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
           todo={todo}
           key={todo.id}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </div>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+  return (
+    <List
+      className="TodoList"
+      width={512}
+      height={512}
+      rowCount={57}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{ outline: 'none' }}
+    />
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
